6dd60d5b0a63af98c2f2e4b507d53f8157f642c9
Daeho Ro <lamanus@outlook.kr>
2024-03-27 15:40:08 +0900

    update execs version, add scoop manifest

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileFullPath = Join-Path $toolsDir "$packageName.exe"
 
-$url64      = 'https://github.com/daeho-ro/execs/releases/download/v0.1.6/execs_0.1.6_windows_amd64.exe'
-$checksum64 = '9b6401b720803444afea75b9f07f8ad9e365fc54766a5c5d4c5fe4a0826140a9'
+$url64      = 'https://github.com/daeho-ro/execs/releases/download/v0.1.7/execs_0.1.7_windows_amd64.exe'
+$checksum64 = 'd46896c6a4696ecc37fb084d9229a169ad84192bdb1c554cc7ba781cb08ba784'
 $checksumType64 = 'sha256'
 
 $packageArgs = @{
